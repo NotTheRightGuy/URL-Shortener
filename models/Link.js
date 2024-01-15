a327a917f0574245d029ea9a036162de05b57bc6
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2024-01-15 20:33:45 +0530

    Added Assignment to Define Shortcode

--- models/Link.js
+++ models/Link.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Link = new Schema({});
+const Link = new Schema({
+    url: {
+        type: String,
+        required: true,
+    },
+    // Define shortCode field here
+});
 
 module.exports = mongoose.model("Link", Link);
