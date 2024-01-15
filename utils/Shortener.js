bb4de26452218865d456ba99b466708f03b80b87
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2024-01-16 01:28:49 +0530

    Add script.js to tabs and implement URL shortening

--- utils/Shortener.js
+++ utils/Shortener.js
@@ -1,5 +1,8 @@
 const CryptoJS = require("crypto-js");
 
-function generateShortURL(longURL) {}
+function generateShortURL(longURL) {
+    const shortCode = CryptoJS.MD5(longURL).toString().slice(0, 6);
+    return shortCode;
+}
 
 module.exports = generateShortURL;
