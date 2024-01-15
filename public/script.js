f74c6dcd07b0fbe377d67246e1e1cb71649de017
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2024-01-16 01:31:34 +0530

    Add URL shortening functionality to script.js

---
+++ public/script.js
@@ -0,0 +1,33 @@
+//! All necessary DOM elements
+const btn = document.getElementById("submit");
+const longLink = document.getElementById("longurl");
+const shortCode = document.getElementById("shortCode");
+const shortUrl = document.getElementById("shortUrl");
+const mainURL = window.location.href;
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const url = longLink.value;
+    const code = shortCode.value;
+    if (url === "") {
+        result.innerHTML = "Please enter a URL";
+    } else {
+        fetch("/shorten", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                url: url,
+                shortCode: code,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                // Implement logic to display the shortened URL
+            })
+            .catch((err) => {
+                result.innerHTML = err;
+            });
+    }
+});
