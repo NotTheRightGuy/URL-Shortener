3a54bf9db76f4ef9b6705affc85e5f10d2e89007
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2024-01-15 23:57:06 +0530

    Update logger.js

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,3 +1,10 @@
-function logger(req, res, next) {}
+function logger(req, res, next) {
+    console.log("=========================================");
+    console.log("Request Time: " + new Date().toISOString());
+    console.log("Request Type: " + req.method);
+    console.log("Request URL: " + req.originalUrl);
+    console.log("Request IP: " + req.ip);
+    next();
+}
 
 module.exports = logger;
