cfa32b8d25b2e18a3c075fe954d95086c9a0da3b
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2024-01-15 22:20:12 +0530

    Added Boilerplate for utils

--- utils/Shortener.js
+++ utils/Shortener.js
@@ -1,5 +1,9 @@
 const CryptoJS = require("crypto-js");
 
-function generateShortURL(longURL) {}
+function generateShortURL(longURL) {
+    // Write logic to generate a short URL
+    // We wil use MD5 hashing algorithm to generate a hash of the long URL
+    // Then just take the first 6 characters of the hash and return it
+}
 
 module.exports = generateShortURL;
