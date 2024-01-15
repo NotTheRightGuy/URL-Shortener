e77f89ce06878aa7343ea2076aaacae75856b980
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2024-01-15 23:55:45 +0530

    Update Link.js

--- models/Link.js
+++ models/Link.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Link = new Schema({});
+const Link = new Schema({
+    shortCode: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+});
 
 module.exports = mongoose.model("Link", Link);
