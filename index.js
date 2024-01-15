9a9d18c52069425b3a121302070a391de51893e1
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2024-01-15 22:20:23 +0530

    Add routes for GET and POST requests

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-
 // Define PORT variable here
+const PORT = 1337;
 
 function callback() {
     // Log a message to the console indicating the server has started
+    console.log(`Listening on PORT ${PORT}`);
 }
 
 // Define a route for GET requests to "/"
+app.get("/", (req, res) => {
+    res.json({ msg: "Hello World" });
+});
 // Define a dynamic route for GET requests to "/:code"
+app.get("/:code", (req, res) => {
+    res.json({ code: req.params.code });
+});
 // Define a route for POST requests to "/shorten"
+app.post("/shorten", (req, res) => {
+    res.json({ msg: "POST successful" });
+});
 
 app.listen(PORT, callback);
